97acdefcc36fe61a3ecae504e7593f4af83edb0d
FrankScarlet <hferfrank@gmail.com>
2021-07-10 00:34:43 +0800

    add antd button

--- antd-demo/src/App.js
+++ antd-demo/src/App.js
@@ -1,3 +1,5 @@
+import React from 'react';
+import { Button } from 'antd';
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,15 +11,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">Button</Button>
       </header>
+      
     </div>
   );
 }
