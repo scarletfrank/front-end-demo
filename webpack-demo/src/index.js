f0c0b69aa8e43c4d5eaa1129d7269d721b55b8cd
FrankScarlet <hferfrank@gmail.com>
2021-07-18 21:36:24 +0800

    add webpack more args

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,12 +1,17 @@
 import _ from 'lodash';
 import $ from "jquery";
+import printMe from './print.js';
+
 
 function component() {
   const element = document.createElement('div');
-
+  const btn = document.createElement('button');
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
   return element;
 }
 
@@ -15,7 +20,7 @@ document.body.appendChild(component());
 // 导入 jQuery
 
 $(function () {
-  $("li:odd").css("backgroundColor", "lightblue");
+  $("li:odd").css("backgroundColor", "yellow");
   $("li:even").css("backgroundColor", function () {
     return "#" + "D97634";
   });
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,15 +1,26 @@
 const path = require('path');
+const webpack = require('webpack')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js'
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   devServer:{
     contentBase: path.join(__dirname, 'dist'),
+    // index: 'index.html',
     compress: true, 
-    port: 9000
-  }
+    port: 9000,
+    open: true, // 自动打开浏览器
+    hot: true, 
+    // 热加载
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
