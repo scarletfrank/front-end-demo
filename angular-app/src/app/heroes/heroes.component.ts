22556c2145aa2ff97cfc3c8ef66aa2149bbcf476
FrankScarlet <hferfrank@gmail.com>
2021-08-10 17:28:19 +0800

    add routing

--- angular-app/src/app/heroes/heroes.component.ts
+++ angular-app/src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -15,18 +14,13 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
     ) {}
   
   ngOnInit(): void {
     this.getHeroes();
   }
   
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add('HeroesComponent: Selected hero id=$(hero.id)')
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
