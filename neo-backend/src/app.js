4b8ab205e085d18bdfc362f4ac3df06ff3854956
FrankScarlet <hferfrank@gmail.com>
2021-08-25 00:23:15 +0800

    express-file-upload

--- neo-backend/src/app.js
+++ neo-backend/src/app.js
@@ -1,8 +1,19 @@
 import logger from 'morgan';
 import express from 'express';
+import fileUpload from 'express-fileupload';
+import cors from 'cors';
+import _ from 'lodash';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
 const app = express();
+
+// enable files upload
+app.use(fileUpload({
+    createParentPath: true
+}));
+
+//add other middleware
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- neo-backend/src/controllers/index.js
+++ neo-backend/src/controllers/index.js
@@ -1,3 +1,4 @@
 // export everything from home.js
 export * from './home';
 export * from './messages';
+export * from './photo';
