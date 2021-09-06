556ab54dfb5798061c113df62b6a8c0113229d73
FrankScarlet <hferfrank@gmail.com>
2021-09-06 11:17:53 +0800

    tweak graphin ( 丰富了节点互动、节点样式、图调整工具栏、水印）

--- react-gdb-app/src/components/Board.js
+++ react-gdb-app/src/components/Board.js
@@ -1,27 +1,74 @@
 import React from 'react';
 import Core from './Core';
-import { Input } from 'antd';
+import { Input, Select, InputNumber  } from 'antd';
 
+const { Option } = Select;
 const { Search } = Input;
 
 class Board extends React.Component{
     constructor(props) {
         super(props);
-        this.state = {date: new Date(), query: "MATCH (n:Person)-[re]-(b) RETURN n, re, b LIMIT 100"};
+        this.state = {
+            date: new Date(), 
+            query: "MATCH (n:Person)-[re]-(b) RETURN n, re, b LIMIT 500", 
+            depth: "2", 
+            limit: 1000,
+            poly: "false",
+            layout: "graphin-force",
+            mini: "false"
+        };
       }
     onSearch = value => {
-        console.log(value);
+        // console.log(value);
+        let cypher = "";
+        let clause = "MATCH (n:Person)-[re*1.." + this.state.depth + "]-(b) where n.name = '";
+        cypher = clause + value + "' RETURN n, re, b LIMIT " + this.state.limit.toString();
         this.setState({
-            query: "MATCH (n:Person)-[re]-(b) where n.name = '" + value + "' RETURN n, re, b LIMIT 100"
+            query: cypher
         })
+    }
+    handleChange = value => {
+        this.setState({depth: value})
+    }
+    onNumChange = value => {
+        console.log('changed', value);
+        this.setState({limit: value})
+    }
+    onPolyChange = value => {
+        this.setState({poly: value})
+    }
+    onLayoutChange = value => {
+        this.setState({layout: value})
     }
     render(){
         const query = this.state.query;
-        console.log(query);
+        const poly = this.state.poly;
+        const layout = this.state.layout;
+        // console.log(query);
         return(
             <div>
-                <Search placeholder="search node" onSearch={this.onSearch} style={{ width: 200 }} />
-                <Core query={query}></Core>
+                <InputNumber min={1} max={10000} defaultValue={1000} onChange={this.onNumChange} />
+                <Select defaultValue="二层" style={{ width: 75 }} onChange={this.handleChange}>
+                    <Option value="1">一层</Option>
+                    <Option value="2">二层</Option>
+                    <Option value="3">三层</Option>
+                    <Option value="4">四层</Option>
+                </Select>
+                <Select defaultValue="单边" style={{ width: 70 }} onChange={this.onPolyChange}>
+                    <Option value="false">单边</Option>
+                    <Option value="true">多边</Option>
+                </Select>
+                <Select defaultValue="渐进力导" style={{ width: 100 }} onChange={this.onLayoutChange}>
+                    <Option value="graphin-force">渐进力导</Option>
+                    <Option value="concentric">同心圆</Option>
+                    <Option value="dagre">层次</Option>
+                    <Option value="circular">环形</Option>
+                    <Option value="mds">降纬</Option>
+                    <Option value="force">D3力导</Option>
+                    <Option value="gForce">G6力导</Option>
+                </Select>
+                <Search placeholder="搜索" onSearch={this.onSearch} style={{ width: 120 }} />
+                <Core query={query} poly={poly} layout={layout}></Core>
             </div>
         )
     }
--- react-gdb-app/src/components/Core.js
+++ react-gdb-app/src/components/Core.js
@@ -1,6 +1,9 @@
-import React, {useEffect } from 'react';
-import Graphin, {Utils} from '@antv/graphin';
-import { MiniMap} from '@antv/graphin-components';
+import React, { useEffect } from 'react';
+import Graphin, { GraphinContext, Utils } from '@antv/graphin';
+import { Tooltip, MiniMap, ContextMenu  } from '@antv/graphin-components';
+import {message, Card} from 'antd';
+import { TagFilled, DeleteFilled, ExpandAltOutlined } from '@ant-design/icons';
+import { WaterMark } from '@ant-design/pro-layout';
 import { useReadCypher } from "use-neo4j";
 
 // 引入资源文件
@@ -14,28 +17,33 @@ import NeoG6 from "./neog6";
 
 
 // 注册到 Graphin 中
-const { fontFamily} = iconLoader();
+const { fontFamily } = iconLoader();
 const icons = Graphin.registerFontFamily(iconLoader);
 
 
-function customStyle(g6Data){
-
-  if (g6Data.nodes.length !== 0){
-    for(const k in g6Data.nodes){
-      const { id, name} = g6Data.nodes[k]
+function customStyle(g6Data, poly) {
+  if (g6Data.nodes.length !== 0) {
+    for (const k in g6Data.nodes) {
+      const {  name } = g6Data.nodes[k]
+      let nodeColor = Math.random() > 0.5 ? 'yellow' : 'blue'
+      let nodeIcon = Math.random() > 0.5 ? icons.eye : icons.user
       g6Data.nodes[k]['style'] = {
-        keyshape: {size:30},
-        label: {value: name, position:'left'},
+        keyshape: { 
+          size: 40 , 
+          fill: nodeColor,
+          fillOpacity: 0.2,
+        },
+        label: { value: name, position: 'right' },
         icon: {
           type: 'font',
           fontFamily: fontFamily,
-          value: icons.user,
+          value: nodeIcon,
         },
         badges: [
           {
             position: 'RT',
             type: 'text',
-            value: id,
+            value: Math.floor(Math.random()),
             size: [15, 15],
             fill: 'red',
             color: '#fff',
@@ -43,27 +51,98 @@ function customStyle(g6Data){
         ],
       }
     }
-    for(const k in g6Data.edges){
-      const {amt} = g6Data.edges[k];
+    for (const k in g6Data.edges) {
+      const { amt } = g6Data.edges[k];
       g6Data.edges[k]['style'] = {
-        label: {value: `${amt}`, fill:'blue', fontSize: 10}, 
-        keyshape: {stroke: 'red', lineWidth: 2}
+        label: { value: `${amt}`, fill: 'blue', fontSize: 10 },
+        keyshape: { 
+          stroke: 'grey', 
+          lineWidth: 2
+        }
       }
     }
-  } 
-  // Utils.processEdges
-  const processEdges = Utils.processEdges([...g6Data.edges], { poly: 50, loop: 10 }) 
+  }
+  if (poly === "true") {
+    // Utils.processEdges
+    const processEdges = Utils.processEdges([...g6Data.edges], { poly: 50, loop: 10 })
+  }
   return g6Data
 }
+
+const CustomTooltip = () => {
+  const { tooltip } = React.useContext(GraphinContext);
+  const context = tooltip.node;
+  const { item } = context;
+  const model = item && item.getModel();
+  let acc = model.acc
+  let pat = /(\d{4})\d*(\d{4})/
+  let desc_acc = acc.replace(pat,'$1****$2');
+  return (
+    // @ts-ignore
+    <div>
+      <Card title="节点信息" style={{ width: '200px' }}>
+        公司: {model.company}
+        <br></br>
+        账户: {desc_acc}
+      </Card>
+    </div>
+  );
+};
+
+const { Menu } = ContextMenu;
+const options = [
+  {
+    key: 'tag',
+    icon: <TagFilled />,
+    name: '打标',
+  },
+  {
+    key: 'delete',
+    icon: <DeleteFilled />,
+    name: '删除',
+  },
+  {
+    key: 'expand',
+    icon: <ExpandAltOutlined />,
+    name: '扩散',
+  },
+];
+const CanvasMenu = () => {
+  const { graph, contextmenu } = React.useContext(GraphinContext);
+  const context = contextmenu.canvas;
+  const handleDownload = () => {
+    graph.downloadFullImage('canvas-contextmenu');
+    context.handleClose();
+  };
+  const handleClear = () => {
+    message.info(`清除画布成功`);
+    context.handleClose();
+  };
+  const handleStopLayout = () => {
+    message.info(`停止布局成功`);
+    context.handleClose();
+  };
+  return (
+    <Menu bindType="canvas">
+      <Menu.Item onClick={handleClear}>清除画布</Menu.Item>
+      <Menu.Item onClick={handleStopLayout}>停止布局</Menu.Item>
+      <Menu.Item onClick={handleDownload}>下载画布</Menu.Item>
+    </Menu>
+  );
+};
+
 
 const Core = (props) => {
   // const data = Utils.mock(10).circle().graphin();
   const { cypher, error, loading, records, run } = useReadCypher(
     props.query
   );
-  useEffect(()=>{
-   run({cypher}) 
+  useEffect(() => {
+    run({ cypher })
   }, [props.query])
+  const handleChange = (menuItem, menuData) => {
+    message.info(`元素：${menuData.id}，动作：${menuItem.name}`);
+  };
 
   // Default to Loading Message
   let result = <div className="ui active dimmer">Loading...</div>;
@@ -75,15 +154,34 @@ const Core = (props) => {
     if (records instanceof Array) {
       g6.toG6Format(records)
     }
-    let styleG6data = customStyle(g6.data)
+    let styleG6data = customStyle(g6.data, props.poly)
     result = (
-      <div>
-        <Graphin height='650' data={styleG6data} layout={{ type:'dagre' } } fitView >
-          <MiniMap visible/>
-        </Graphin>
-      </div>
+      <WaterMark content="scarlet">
+            <Graphin height='750' data={styleG6data} layout={{ type: props.layout }} fitView >
+            <MiniMap visible/>
+            <Tooltip bindType="node">
+              <CustomTooltip />
+            </Tooltip>
+            <ContextMenu style={{ width: '80px' }}>
+              <Menu options={options} onChange={handleChange} bindType="node" />
+            </ContextMenu>
+            <ContextMenu style={{ width: '80px' }} bindType="canvas">
+              <CanvasMenu />
+            </ContextMenu>
+            <ContextMenu style={{ width: '120px' }} bindType="edge">
+              <Menu
+                options={options.map(item => {
+                  return { ...item, name: `${item.name}-EDGE` };
+                })}
+                onChange={handleChange}
+                bindType="edge"
+              />
+            </ContextMenu>
+            </Graphin>
+      </WaterMark>
     );
   }
+
   return (
     <div className="App">
       <pre>{cypher}</pre>
